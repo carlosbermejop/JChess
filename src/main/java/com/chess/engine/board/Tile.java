e5b9266414e417ae5d4acefd0a33c2378c2e662e
Carlos Bermejo Pérez <carlos.bermejo@oup.com>
2022-01-07 11:29:32 +0100

    #2 finished + Maven support added

--- src/main/java/com/chess/engine/board/Tile.java
+++ src/main/java/com/chess/engine/board/Tile.java
@@ -1,6 +1,28 @@
-public abstract class Tile {
+package com.chess.engine.board;
 
-    int tileCoordinate;
+import com.chess.engine.pieces.Piece;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.google.common.collect.ImmutableMap;
+
+public abstract class Tile {
+    protected final int tileCoordinate;
+
+    private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+
+        for(int i = 0; i < 64; i++) {
+            emptyTileMap.put(i, new EmptyTile(i));
+        }
+        return ImmutableMap.copyOf(emptyTileMap);
+    }
+
+    public static Tile createTile(final int tileCoordinate, final Piece piece) {
+        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES.get(tileCoordinate);
+    }
 
     Tile(int tileCoordinate) {
         this.tileCoordinate = tileCoordinate;
@@ -27,7 +49,7 @@ public abstract class Tile {
     }
 
     public static final class OccupiedTile extends Tile {
-        Piece pieceOnTile;
+        private final Piece pieceOnTile;
 
         OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
             super(tileCoordinate);
